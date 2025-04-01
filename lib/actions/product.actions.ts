1860ae2f15d28b4ae88cba56c3e22a7b6aabe7d3
Alexander S <70532114+autumnjava@users.noreply.github.com>
2025-04-01 12:39:17 +0200

    initial

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,16 +1,18 @@
 'use server';
 
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '@/db/prisma';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
 import { convertToPlainObject } from '../utils';
 
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: 'desc' },
   });
 
   return convertToPlainObject(data);
+}
+
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({ where: { slug } });
 }
