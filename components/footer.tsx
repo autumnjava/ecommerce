1115525ef474e6b15c546e848d74062ba39d8bfa
Alexander S <70532114+autumnjava@users.noreply.github.com>
2025-04-07 14:59:52 +0200

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="border-t">
       <div className="flex-center p-5">
-        {currentYear} {APP_NAME}. All rights reserver
+        {currentYear} {APP_NAME}. All rights reserved
       </div>
     </footer>
   );
